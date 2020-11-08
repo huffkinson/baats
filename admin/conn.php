039c88bb8d5f0e0ce21d63c5963565c0d8e603dc
huffkinson <kvbt2002@gmail.com>
2020-11-08 18:24:33 +0800

    modified conn file

--- admin/conn.php
+++ admin/conn.php
@@ -1,5 +1,7 @@
 <?php
-	$connection = new mysqli('localhost','boteacts-client','TLKTb!QN8t1}','boteacts_db') or die();
+	
+	$connection = new mysqli('billing.boacts.com','boteacts-user@boacts.com','TLKTb!QN8t1}','boteacts_db') or die();
+	// $connection = new mysqli('localhost','boteacts-client','TLKTb!QN8t1}','boteacts_db') or die();
 	//$connection = new mysqli('localhost','id12256582_bote','bote2020','id12256582_bote') or die();
 	//$connection = new mysqli('localhost','root','','samplecrud') or die(mysqli_error());
 ?>
